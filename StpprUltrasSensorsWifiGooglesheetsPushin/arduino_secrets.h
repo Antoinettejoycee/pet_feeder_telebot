71529f7f0814edf874c90b1ff58a7b6a75d30972
Antoinettejoycee <s00291088@atu.ie>
2026-04-27 11:27:12 +0100

    Delete StpprUltrasSensorsWifiGooglesheetsPushin directory

--- StpprUltrasSensorsWifiGooglesheetsPushin/arduino_secrets.h
+++
@@ -1,8 +0,0 @@
-// arduino_secrets.h
-// Store sensitive data here (never upload this file to public repos)
-
-#define SECRET_SSID     "iPhone"
-#define SECRET_PASS     "12345678"
-
-#define SECRET_TELEGRAM_TOKEN "8618434024:AAE4aUhvIW6RMpW1Pq4MIvoyZ-xZaWheVq8"
-#define SECRET_CHAT_ID "8781224818"
